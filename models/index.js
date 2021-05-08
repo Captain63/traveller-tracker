d7f852778c863b49c66cebcc4cd383f0c256fe01
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-08 13:42:12 -0400

    Update many-to-many relationship

--- models/index.js
+++ models/index.js
@@ -3,22 +3,14 @@ const Location = require("./Location");
 const Traveller = require("./Traveller");
 const Trip = require("./Trip");
 
-Trip.hasMany(Traveller, {
-    foreignKey: "traveller_id",
-    onDelete: "CASCADE"
+Traveller.belongsToMany(Location, {
+    through: { model: Trip },
+    uniqueKey: false
 })
 
-Trip.hasMany(Location, {
-    foreignKey: "location_id",
-    onDelete: "CASCADE"
-})
-
-Traveller.belongsTo(Trip, {
-    foreignKey: "traveller_id",
-})
-
-Location.belongsTo(Trip, {
-    foreignKey: "location_id"
+Location.belongsToMany(Traveller, {
+    through: { model: Trip },
+    uniqueKey: false
 })
 
 module.exports = { Location, Traveller, Trip };
